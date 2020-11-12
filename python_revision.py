5ef5699a59e4afde275732060d1e5920d1a87d2f
ldaijiw <lwaltmann@spartaglobal>
2020-11-12 10:32:19 +0000

    task 2

--- python_revision.py
+++ python_revision.py
@@ -1,4 +1,11 @@
 # Declare a list with numbers 1 to 5 and add 6 at the end
-
 new_list = [1, 2, 3, 4, 5]
 new_list.append(6)
+
+# Create a tuple with numbers 1 to 5 and print up to 3
+new_tuple = (1, 2, 3, 4, 5)
+
+for num in new_tuple:
+    if num > 3:
+        break
+    print(num)
