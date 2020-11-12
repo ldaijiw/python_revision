3a6cee7e3b913fa08e02ab9dcf1bb118c047db07
ldaijiw <lwaltmann@spartaglobal>
2020-11-12 10:54:00 +0000

    more questions

--- python_revision.py
+++ python_revision.py
@@ -21,3 +21,22 @@ print(shopping_dict["bread"])
 # replace price for one of the items
 shopping_dict["bread"] = 2
 print(shopping_dict["bread"])
+
+
+# Declare function that takes two arguments to add 2 numbers together
+def add(num1, num2):
+    return num1 + num2
+
+print(add(2,4))
+
+
+# Create a class called person with name and age
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+new_person = Person("Leo", 21)
+print(new_person.name)
+print(new_person.age)
